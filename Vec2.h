3cb0a896b3af9b2d0b7697aa39774bbd1d4fc5bc
Kevin <kzhang366@gmail.com>
2025-05-12 15:41:53 -0600

    vector class

--- Vec2.h
+++ Vec2.h
@@ -6,8 +6,37 @@
 // 2d vector class
 struct Vec2 {
     Vec2() {} // default constructor 
-    Vec2(float x, float y) : x(x), y(y) {} // parameterized constructor 
+    
+    Vec2(float x, float y) : x(x), y(y) {} // parameterized constructor
+    
+    /* set the x and y pos */
+    void Set(float x_val, float y_val) {
+        x = x_val;
+        y = y_val;
+    }
+
+    float Length() {
+        return sqrtf(x * x + y * y);
+    }
+
+    /* define the operators on Vec2 data type */
+    void operator -= (Vec2 &vec) {
+        x -= vec.x;
+        y -= vec.y;
+    }
+    
+    void operator += (Vec2 &vec) {
+        x += vec.x;
+        y += vec.y;
+    }
+    
+    void operator *= (float val) {
+        x *= val;
+        y *= val;
+    }
+
     float x, y; 
+    
 };
 
 #endif
