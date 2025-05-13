f736c0750609ce539a5627e8d1f3e43241c62b64
Kevin <kzhang366@gmail.com>
2025-05-13 14:37:33 -0600

    making strides rn

--- Sim.cpp
+++ Sim.cpp
@@ -6,24 +6,28 @@
 
 Body Bodies[NUM_BODIES];
 
+/* initialize the bodies */
 void InitBodies(void) {
     for (int i = 0; i < NUM_BODIES; ++i) {
-        Bodies[i] = Body();
+        Vec2 p, v;
+        p = Vec2((rand() % 10) + 1, (rand() % 10) + 1);
+        v = Vec2(0, 0);
+        Bodies[i] = Body(p, v, 1);
     }
 } 
 
 Body ComputeForce(Body& b) {
     Vec2 p, v;
-    float m;
 
     p = Vec2(0, 0);
     v = Vec2(0, b.mass * -9.81);
-    m = 0;
 
-    return Body(p, v, m);
+    return Body(p, v, 0);
 }
 
 int main(void) {
+    Body b;
+
     std::cout << "SIMULATION:\n";
 
 
