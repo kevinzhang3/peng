2c487755d96ebcc4c5edd97c03a08333b04ffc63
Kevin <kzhang366@gmail.com>
2025-05-14 16:54:46 -0600

    lol

--- src/Collide.cpp
+++ src/Collide.cpp
@@ -1,12 +1,16 @@
 #include <Body.h>
 #include <Util.h>
 
-int CollideBody(Body& a, Body& b) {
+
+
+int CollideBody(Body& bodyA, Body& bodyB) {
     // explosion
     return 0;
 }
 
-int CollideBarrier(Body& a) {
-    
+int CollideBarrier(Body& body) {
+    if (body.position.x + body.radius >= X_BORDER) {
+        //
+    }
     return 0;
 }
