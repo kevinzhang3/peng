ca1e07a76db89318d74a4a180900fbb0b045926d
Kevin <kzhang366@gmail.com>
2025-05-15 12:11:34 -0600

    ok

--- include/Body.h
+++ include/Body.h
@@ -2,22 +2,18 @@
 #define BODY_H
 
 #include "Vec2.h"
-#include "Shape.h"
 
 struct Body {
     Body() {}
 
-    Body(Vec2 p, Vec2 v, Shape s) {
+    Body(Vec2 p, Vec2 v) {
         position = p;
         l_velocity = v;
-        shape = s;
         torque = 0;
         angle = 0;
         a_velocity = 0;
     } 
     
-    Shape shape;
-
     Vec2 position;
     
     Vec2 l_velocity;
@@ -30,7 +26,27 @@ struct Body {
     
     float a_velocity;
 
+    float mass;
 
+    float m_inertia;
+
+};
+
+struct Box : public Body {
+    Box() {};
+
+    Box(float h, float w, float m, float mi) {
+        height = h;
+        width = w;
+        mass = m;
+        m_inertia = m * (w * w + h * h) / 12;
+    }
+
+    float height, width;
+};
+
+struct Circle : public Body {
+    float radius;
 };
 
 #endif
