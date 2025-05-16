60555c240d65e1c98068429aeaf72b9e61c20afe
kevinzhang3 <kzhang366@gmail.com>
2025-05-16 11:51:46 -0600

    init a window

--- src/Sim.cpp
+++ src/Sim.cpp
@@ -3,7 +3,7 @@
 #include <Vec2.h>
 #include <Body.h>
 #include <Util.h>
-#include <glad/gl.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 
@@ -12,8 +12,41 @@
 
 Body bodies[NUM_BODIES];
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
 int main(void) {
-    std::cout << "SIMULATION:\n";
+    std::cout << "Init window...\n";
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Simulation", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to init GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+    
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // render loop
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
     return 0;
 }
