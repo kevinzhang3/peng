c1471583425e59681086b60e09faaf41d4e6d408
Kevin <kzhang366@gmail.com>
2025-05-22 11:52:46 -0600

    fragment shader

--- src/Sim.cpp
+++ src/Sim.cpp
@@ -18,6 +18,12 @@ const char *vertexShaderSource = "#version 330 core\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+    "}\0";
+
 Body bodies[NUM_BODIES];
 
 // for dynamically resizing the window
@@ -61,25 +67,36 @@ int main(void) {
         0.5f, 0.5f, 0.0f
     };
 
-    // vertex buffer and shader obj ids
+    // vertex buffer ref id
     unsigned int VBO;
+    
+    // shader ref ids
     unsigned int vertexShader;
+    unsigned int fragmentShader;
     int success;
     char infoLog[512];
 
    
-    // generating buffer, binding it, and putting our vertices into the buf
+    // generating buffer id, binding it, and putting our vertices into the buf
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
-    // attach our shader obj and compile it 
+    // generate id, compile shader  
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(vertexShader);
+    glCompileShader(fragmentShader);
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if (success) {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTeX::COMPILATION_FAILED\n" << infoLog <<std::endl;
+    }
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTeX::COMPILATION_FAILED\n" << infoLog <<std::endl;
     }
 
