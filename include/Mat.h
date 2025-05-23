c6d0f712f50d8b289d99fded3bf3c3a8a4dbc24c
Kevin <kzhang366@gmail.com>
2025-05-23 14:29:15 -0600

    done fo today

--- include/Mat.h
+++ include/Mat.h
@@ -1,7 +1,8 @@
 #ifndef MAT_H
 #define MAT_H
 
-#include "Vec2.h"
+#include <cmath>
+#include <Vec2.h>
 
 struct Mat4 {
     float data[16];
@@ -24,6 +25,14 @@ struct Mat4 {
     static inline Mat4 scale(Mat4 mat, const Vec2& v) {
         mat.data[0] = v.x;
         mat.data[5] = v.y;
+        return mat;
+    }
+
+    static inline Mat4 rotate(Mat4 mat, float rad) {
+        mat.data[0] = cos(rad);
+        mat.data[1] = -sin(rad);
+        mat.data[4] = sin(rad);
+        mat.data[5] = cos(rad);
         return mat;
     }
 
