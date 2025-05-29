8f7a72902f3561de9a7d4962cffff14bbcf89e13
kevinzhang3 <kzhang366@gmail.com>
2025-05-29 01:03:22 -0600

    gravity

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -9,35 +9,42 @@ int main() {
     float simTime = 10;
     float currTime = 0;
     
-    int step = 500000; // time step (500 000 microsec == 0.5 sec)
-    float dt = (float)step / 1000000;
+    int step = 1000000; // time step (500 000 microsec == 0.5 sec)
+    float dt = 1;
 
     InitBodies();
     while (currTime < simTime) {
+        
+        usleep(step);
+        
         for (int i = 0; i < NUM_BODIES; ++i) {
             Box *body = &Bodies[i];
-            Vec2 gravityForce = Vec2(0, 9.81*body->mass);
             
-            body->force += gravityForce;
+            Vec2 gravityForce = Vec2(0, -9.81*body->mass);
+           
+            // set force and acceleration for linear and angular
+            body->force = gravityForce;
             body->torque = body->ComputeTorque(gravityForce, Vec2()); // apply gravity at centre of mass
             body->l_accel = body->ComputeLinearAccel(body->force);
+            body->a_accel = body->ComputeRotAccel(body->torque);
+
+            // calc the new velocity and the avg
+            Vec2 oldVelocity = body->l_velocity;
+            body->l_velocity += body->l_accel * dt;
+            Vec2 avgVelocity = (oldVelocity + body->l_velocity) / 2; // could use Euler integration here instead 
+                                                                     // maybe for smaller time step?
             
-            // calc the new velocity
-            Vec2 newVel = body->l_accel * dt;
-            body->l_velocity += newVel;
-            
-            // calc the new position based on new velocity
-            Vec2 newPos = body->l_velocity * dt;
-            body->position += newPos;
+            // calc the new position
+            body->position += avgVelocity * dt;
 
             // same for rotation (but luckily they are not vectors lol)
             body->a_velocity += body->a_accel * dt;
             body->angle += body->a_velocity * dt;
 
         }
+        
         PrintBodies(); 
         currTime += dt;
-        usleep(step);
     }
 
 }
