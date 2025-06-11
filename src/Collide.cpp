4813e3f2ea1e87eb74ace875e906ead5a448de1f
Kevin <kzhang366@gmail.com>
2025-06-11 10:48:56 -0600

    working on projections

--- src/Collide.cpp
+++ src/Collide.cpp
@@ -4,6 +4,18 @@
 int DetectCollisionBox(Box& boxA, Box& boxB) {
     boxA.ComputeCorner();
     boxB.ComputeCorner();
+
+    Vec2 projA[4];
+    Vec2 projB[4];
+    Vec2 axis[4];
+
+    // calculate the axes 
+    axis[0] = boxA.corner[0] - boxA.corner[1];
+    axis[1] = boxA.corner[1] - boxA.corner[3];
+    axis[2] = boxA.corner[0] - boxA.corner[2];
+    axis[3] = boxA.corner[2] - boxA.corner[3];
+
+    return 0;
 }
 
 int CollideBody(Body& bodyA, Body& bodyB) {
